b25e941a6ee293626c446b0b54cdd72063be0ec9
Stanislav Matveew <stanislav.matveew@yandex.ru>
2022-09-16 01:58:00 +0300

    обернул etl в backoff

--- etl/simple_project/app/postgres_to_es/load_data.py
+++ etl/simple_project/app/postgres_to_es/load_data.py
@@ -2,23 +2,31 @@ import datetime
 import os
 import time
 
+import elasticsearch
+import psycopg2
+
 from ETL_classes.extractor import Extractor
 from ETL_classes.loader import Loader
 from ETL_classes.transformer import Transformer
+from etl.simple_project.app.postgres_to_es.utils.backoff_util import backoff
 from storage import (State, JsonFileStorage)
 from utils.env_utild import return_dsn
 from utils.logger_util import get_logger
 
 
-def etl(logger, extracrot, transformer, state):
+@backoff((elasticsearch.exceptions.ConnectionError,))
+@backoff((psycopg2.OperationalError,))
+def etl(logger, extracrot, transformer, state, loader):
     '''
     Extract-Transform-Load процесс перекачки данных из PostgreSQL в Elasticsearch
     '''
 
     last_sync_timestamp = state.get_state('last_sync_timestamp')
-    logger.info(f'last_sync_timestamp = {last_sync_timestamp}')
+    logger.info(f'последняя синхронизация была {last_sync_timestamp}')
     start_timestamp = datetime.datetime.now()
     filmwork_ids = state.get_state('filmwork_ids')
+
+    loader.create_index('movies')
 
     for extracted_part in extracrot.extract(last_sync_timestamp, start_timestamp, filmwork_ids):
         data = transformer.transform(extracted_part)
@@ -40,6 +48,6 @@ if __name__ == '__main__':
     loader = Loader(dsn=dsn['es'], logger=logger)
 
     while True:
-        etl(logger, extractor, transformer, state)
+        etl(logger, extractor, transformer, state, loader)
         logger.info(f'в сон на {float(os.environ.get("ES_SLEEP"))}')
         time.sleep(float(os.environ.get('ES_SLEEP')))
