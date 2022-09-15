c8e1fca69362ed6a7d9c77f3b9da46224f7fa36c
Stanislav Matveew <stanislav.matveew@yandex.ru>
2022-09-16 01:02:02 +0300

    косметические изменения extractor.py

--- etl/simple_project/app/postgres_to_es/ETL_classes/extractor.py
+++ etl/simple_project/app/postgres_to_es/ETL_classes/extractor.py
@@ -20,8 +20,8 @@ class Extractor:
         Метод чтения данных пачками.
         После падения чтение начинается с последней обработанной записи
         """
-        with postgres_connection(self.dsn) as pg_conn,pg_conn.cursor() as cursor:
-            sql = """
+        with postgres_connection(self.dsn) as pg_conn, pg_conn.cursor() as cursor:
+            sql = f"""
                     SELECT 
                         fw.id,
                         fw.rating as imdb_rating, 
@@ -31,8 +31,8 @@ class Extractor:
                         string_agg(DISTINCT CASE WHEN pfw.role = 'director' THEN p.full_name ELSE '' END, ',') AS director,
                         array_remove(COALESCE(array_agg(DISTINCT CASE WHEN pfw.role = 'actor' THEN p.full_name END) FILTER (WHERE p.full_name IS NOT NULL)), NULL) AS actors_names,
                         array_remove(COALESCE(array_agg(DISTINCT CASE WHEN pfw.role = 'writer' THEN p.full_name END) FILTER (WHERE p.full_name IS NOT NULL)), NULL) AS writers_names,
-                        concat('[', string_agg(DISTINCT CASE WHEN pfw.role = 'actor' THEN json_build_object('id', p.id, 'name', p.full_name) #>> '{}' END, ','), ']') AS actors,
-                        concat('[', string_agg(DISTINCT CASE WHEN pfw.role = 'writer' THEN json_build_object('id', p.id, 'name', p.full_name) #>> '{}' END, ','), ']') AS writers,
+                        concat('[', string_agg(DISTINCT CASE WHEN pfw.role = 'actor' THEN json_build_object('id', p.id, 'name', p.full_name) #>> '{{}}' END, ','), ']') AS actors,
+                        concat('[', string_agg(DISTINCT CASE WHEN pfw.role = 'writer' THEN json_build_object('id', p.id, 'name', p.full_name) #>> '{{}}' END, ','), ']') AS writers,
                         GREATEST(MAX(fw.modified), MAX(g.modified), MAX(p.modified)) AS last_modified
                     FROM 
                         content.film_work as fw
@@ -41,22 +41,19 @@ class Extractor:
                         LEFT JOIN content.person_film_work pfw ON fw.id = pfw.film_work_id
                         LEFT JOIN content.person p ON pfw.person_id = p.id
                     GROUP BY fw.id
-                    HAVING GREATEST(MAX(fw.modified), MAX(g.modified), MAX(p.modified)) > %(extract_timestamp)s
+                    
                     """
 
             if exclude_ids:
-                sql += """
-                AND (fw.id not in %(exclude_ids)s OR 
-                  GREATEST(MAX(fw.modified), MAX(g.modified), MAX(p.modified)) > %(start_timestamp)s)
+                sql += f"""
+                AND (fw.id not in {tuple(exclude_ids)} OR 
+                  GREATEST(MAX(fw.modified), MAX(g.modified), MAX(p.modified)) > '{str(start_timestamp)}')
                 """
-            sql += """
-            ORDER BY last_modified DESC;
+            sql += f"""
+            HAVING GREATEST(MAX(fw.modified), MAX(g.modified), MAX(p.modified)) > '{str(extract_timestamp)}'
+            ORDER BY GREATEST(MAX(fw.modified), MAX(g.modified), MAX(p.modified)) DESC;
             """
-            cursor.execute(sql, {'exclude_ids': tuple(exclude_ids),
-                                 'extract_timestamp': extract_timestamp,
-                                 'start_timestamp': start_timestamp
-                                 }
-                           )
+            cursor.execute(sql)
 
             while True:
                 rows = cursor.fetchmany(self.chunk_size)
