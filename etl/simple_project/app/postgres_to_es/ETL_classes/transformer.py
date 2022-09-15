6f43a639666188005b96cfdb01a611fed7e023dd
Stanislav Matveew <stanislav.matveew@yandex.ru>
2022-09-15 23:09:18 +0300

    косметические изменения transformer.py

--- etl/simple_project/app/postgres_to_es/ETL_classes/transformer.py
+++ etl/simple_project/app/postgres_to_es/ETL_classes/transformer.py
@@ -2,12 +2,12 @@ import json
 
 
 class Transformer:
-    def transform(self, data: dict) -> list[dict]:
+    def transform(self, extracted_part: dict) -> list[dict]:
         """Обработка данных из Postgres и преобразование в формат для ElasticSearch
         :param data: Словарь из Postgres
         """
-        butch = []
-        for row in data:
+        transformed_part = []
+        for row in extracted_part:
             filmwork = {
                 "id": row['id'],
                 "imdb_rating": row['imdb_rating'],
@@ -20,5 +20,5 @@ class Transformer:
                 "actors": json.loads(row['actors']) if row['actors'] is not None else [],
                 "writers": json.loads(row['writers']) if row['writers'] is not None else []
             }
-            butch.append(filmwork)
-        return butch
+            transformed_part.append(filmwork)
+        return transformed_part
